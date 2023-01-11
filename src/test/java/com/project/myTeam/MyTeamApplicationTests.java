4f6f1460db70b3698a0057ba3441f04e9aa37f47
MuhtasimMahmud <muhtasim.mzs16@gmail.com>
2023-01-12 02:48:54 +0600

    working

---
+++ src/test/java/com/project/myTeam/MyTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.myTeam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
