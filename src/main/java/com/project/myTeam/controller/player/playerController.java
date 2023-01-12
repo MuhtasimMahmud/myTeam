281093c437df60a42b237e1da8702622dcaa916b
MuhtasimMahmud <muhtasim.mzs16@gmail.com>
2023-01-12 06:28:44 +0600

    updated

--- src/main/java/com/project/myTeam/controller/player/playerController.java
+++ src/main/java/com/project/myTeam/controller/player/playerController.java
@@ -1,18 +1,30 @@
 package com.project.myTeam.controller.player;
 
 
+import com.project.myTeam.models.User;
+import com.project.myTeam.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/player")
 public class playerController {
 
+    @Autowired
+    UserRepository userRepository;
 
 
     @GetMapping("/playerDashboard")
-    public String normalUser(){
+    public String normalUser(Model model, Principal principal){
+
+        User player = userRepository.findByEmail(principal.getName());
+
+        model.addAttribute("loggedInUserName", player.getName());
         return "player/playerDashboard";
     }
 
--- src/main/java/com/project/myTeam/controller/teamManagement/managerController.java
+++ src/main/java/com/project/myTeam/controller/teamManagement/managerController.java
@@ -2,8 +2,12 @@ package com.project.myTeam.controller.teamManagement;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/teamManagement")
@@ -11,7 +15,9 @@ public class managerController {
 
 
     @GetMapping("/managerDashboard")
-    public String adminUser(){
+    public String adminUser(Model model, Principal principal){
+
+        model.addAttribute("loggedInUserName", principal.getName());
         return "teamManagement/managerDashboard";
     }
 
