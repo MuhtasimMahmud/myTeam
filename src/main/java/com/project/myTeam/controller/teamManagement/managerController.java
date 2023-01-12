e891b632254cb3c1d89771c8c0c097abe236abfc
MuhtasimMahmud <muhtasim.mzs16@gmail.com>
2023-01-12 06:34:23 +0600

    updated

--- src/main/java/com/project/myTeam/controller/teamManagement/managerController.java
+++ src/main/java/com/project/myTeam/controller/teamManagement/managerController.java
@@ -1,6 +1,9 @@
 package com.project.myTeam.controller.teamManagement;
 
 
+import com.project.myTeam.models.User;
+import com.project.myTeam.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,11 +16,15 @@ import java.security.Principal;
 @RequestMapping("/teamManagement")
 public class managerController {
 
+    @Autowired
+    UserRepository userRepository;
 
     @GetMapping("/managerDashboard")
     public String adminUser(Model model, Principal principal){
 
-        model.addAttribute("loggedInUserName", principal.getName());
+        User teamManagement = userRepository.findByEmail(principal.getName());
+
+        model.addAttribute("loggedInUserName", teamManagement.getName());
         return "teamManagement/managerDashboard";
     }
 
