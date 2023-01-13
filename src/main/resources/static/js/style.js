dcb11fce08689604d086fa880d1ec740348c22f4
MuhtasimMahmud <muhtasim.mzs16@gmail.com>
2023-01-14 03:13:58 +0600

    updated

--- src/main/resources/static/js/style.js
+++ src/main/resources/static/js/style.js
@@ -1,5 +1,7 @@
 function highlight(ElementId){
 
+    // document.getElementsByTagName("a").background = "#37474F";
+
     console.log(ElementId);
     document.getElementById(ElementId).style.background = "#1c2828";
 }
