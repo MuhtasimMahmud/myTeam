0a132d484b9d2affe05371134f6ee6c2f234035b
MuhtasimMahmud <muhtasim.mzs16@gmail.com>
2023-01-17 05:44:56 +0600

    updated

--- src/main/resources/static/js/style.js
+++ src/main/resources/static/js/style.js
@@ -2,6 +2,18 @@ function highlight(ElementId){
 
     // document.getElementsByTagName("a").background = "#37474F";
 
-    console.log(ElementId);
+    // console.log(ElementId);
+    // document.getElementById(ElementId).style.background = "#1c2828";
+
+
+
+    var el = document.querySelectorAll('a');
+
+    for (var i = 0; i < el.length; i++) {
+        var currentEl = el[i];
+        currentEl.style.background = "#37474F";
+    }
+
     document.getElementById(ElementId).style.background = "#1c2828";
+
 }
